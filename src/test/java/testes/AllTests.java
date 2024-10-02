f81fb8257418b20fe5eda1c6b1c76085d0e7c274
João Victor Porto <140113109+Jportov@users.noreply.github.com>
2024-10-02 14:14:03 -0300

    Update AllTests.java

--- src/test/java/testes/AllTests.java
+++ src/test/java/testes/AllTests.java
@@ -4,10 +4,6 @@ package testes;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-/**
- * @author rodrigo.pires
- *
- */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({ ClienteServiceTest.class, ClienteDAOTest.class,
 	ProdutoServiceTest.class, ProdutoDAOTest.class,
